5cb0cd21229aad7d211d67611e3f33a3de117fce
Gerrit Hoevers <gerrithoevers@ziggo.nl>
2022-12-04 20:51:09 +0100

    test

--- 2022/01/code.py
+++ 2022/01/code.py
@@ -1,22 +1,21 @@
-from day_base import Day
+# https://adventofcode.com/2022/day/1
 
+with open('./input.txt', 'r') as f:
+    text = f.read()
 
-class Day1(Day):
-    def __init__(self):
-        super().__init__(2022, 1, "Calorie Counting", debug=True, expected_a=24000, expected_b=45000)
+deers = [
+    sum([
+        int(cal)
+        for cal in deer.split('\n')
+        if cal != ''
+    ])
+    for deer in text.split('\n\n')
+]
 
-    def parse(self, puzzle_input):
-        individual_calories = [list(map(int, calories_per_elf.split('\n')))
-                               for calories_per_elf in puzzle_input.split('\n\n')]
-        return list(map(sum, individual_calories))
+# Part 1
+max_calories = max(deers)
+print(f'Part 1 : {max_calories}')
 
-    def part_a(self, parsed_puzzle_input):
-        return max(parsed_puzzle_input)
-
-    def part_b(self, parsed_puzzle_input):
-        sorted_calories_per_elf = sorted(parsed_puzzle_input, reverse=True)
-        return sum(sorted_calories_per_elf[:3])
-
-
-if __name__ == '__main__':
-    Day1().run()
+# Part 2
+sum_of_top3_calories = sum(sorted(deers)[-3:])
+print(f'Part 2 : {sum_of_top3_calories}')

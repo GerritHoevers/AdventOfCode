7c9b666b9d489db63de1b3de74c7a1c1742f342f
Gerrit Hoevers <gerrithoevers@ziggo.nl>
2022-12-13 21:04:50 +0100

    solved day 5 on day 13 ... keep up the good work!

--- 2022/05/code.py
+++ 2022/05/code.py
@@ -3,63 +3,110 @@
 import pathlib
 import sys
 
-staples = 3
-
 def parse(puzzle_input):
     """Parse input"""
-    data = []
-    moves = []
-    for line in puzzle_input.split('\n'):
-        #print(line, len(line))
-        index = 0
-        row = 0
-        symbol = line[index+1]
-        if symbol !=1:
-            # line contains crate data
-            while index+3 < len(line) and symbol != ' ':
-                print("index: ", index, "symbol: ", symbol, "row: ", row)
-                data[row].append(symbol)
-                index += 4
-                row += 1
-            else:
-                break
+    stacks = [[]]
+    for i in range(8):
+        stacks.append([])
+    moves = [[]]
+    lines = [line for line in puzzle_input.split('\n')]
+    line_number = 0
+    stack_number = 0
+    pos = 1
+    value = lines[line_number][pos]
+    while value != '1':
+        #reading stack data
+        if value != ' ':
+            #there is a value on this position
+            print("stack_number: ", stack_number, "value: ", value)
+            stacks[stack_number].append(value)
+        pos += 4
+        stack_number += 1
+        if pos > len(lines[line_number]):
+            pos = 1
+            stack_number = 0
+            line_number += 1
+        value = lines[line_number][pos]
+    # reverse items in the stacks
+    print("Na het inlezen zijn er ", len(stacks), " stacks")
+    for i in range(0, len(stacks)):
+        stacks[i].reverse()
 
-    #data = [['Z', 'N'], ['M', 'C', 'D'], ['P'], [1,2,1], [3,1,3], [2,2,1], [1,1,2]]
-    print(data)
-    return data, moves
+    #arriving at the line with the stack numbers 
+    line_number += 2 #skip 2 lines
+    move_number = 0
+    while line_number < len(lines):
+        values = [value for value in lines[line_number].split(' ')]
+        #print(values)
+        moves[move_number].append(values[1])
+        moves[move_number].append(values[3])
+        moves[move_number].append(values[5])
+        #print("move_number: ", move_number, "is ", moves[move_number])
+        line_number += 1
+        move_number += 1
+        moves.append([])
+
+    return stacks, moves
 
 def part1(data):
     """Solve part 1"""
-    rows = []
-    for i in range(0, staples):
-        rows.append(data[i]) 
-    moves = []
-    for i in range(staples, len(data)):
-        moves.append(data[i]) 
-    #print(moves)
-    for i in range(0, len(moves)):
-        number_to_move = moves[i][0]
-        from_staple = moves[i][1] - 1
-        to_staple = moves[i][2] - 1
-        #print("move ", number_to_move, " from ", from_staple, " to ", to_staple)
+    print("solving part 1 ...")
+    stacks, moves = data
+    #print("moves: ", moves)
+    #print("number of moves: ", len(moves))
+    for i in range(0, len(moves)-1):
+        print("stacks before move: ", stacks)
+        number_to_move = int(moves[i][0])
+        from_staple = int(moves[i][1]) - 1 #in code stapels are numbered starting with 0
+        to_staple = int(moves[i][2]) - 1    
+        print("move ", number_to_move, " from ", from_staple + 1, " to ", to_staple + 1)
         for j in range(0, number_to_move):
-            rows[to_staple].append(rows[from_staple].pop())
-        #print(rows)
+            stacks[to_staple].append(stacks[from_staple].pop())
+        print("stacks after move: ", stacks)
 
     solution = ''
-    for i in range(0,staples):
-        text = rows[i][len(rows[i])-1]
+    print("Er zijn ", len(stacks), " stapels")
+    for i in range(0,len(stacks)):
+        print("stack number: ", i, "inhoud is: ", stacks[i], " lengte is: ", len(stacks[i]))
+        text = stacks[i][len(stacks[i])-1]
+        print(" last digit is: ", text)
         solution += text
     return solution
 
 def part2(data):
     """Solve part 2"""
+    print("\n\nsolving part 2 ...")
+    stacks, moves = data
+    #print("moves: ", moves)
+    #print("number of moves: ", len(moves))
+    for i in range(0, len(moves)-1):
+        print("stacks before move: ", stacks)
+        number_to_move = int(moves[i][0])
+        from_staple = int(moves[i][1]) - 1 #in code stapels are numbered starting with 0
+        to_staple = int(moves[i][2]) - 1    
+        print("move ", number_to_move, " from ", from_staple + 1, " to ", to_staple + 1)        
+        for j in range(number_to_move, 0, -1):
+            print("from_staple is ", stacks[from_staple], " with length ", len(stacks[from_staple]))
+            k = len(stacks[from_staple])-j
+            print("now getting nr ", k, " from staple ", from_staple + 1)
+            stacks[to_staple].append(stacks[from_staple].pop(k) )
+        
+        print("stacks after move: ", stacks)
 
+    solution = ''
+    print("Er zijn ", len(stacks), " stapels")
+    for i in range(0,len(stacks)):
+        print("stack number: ", i, "inhoud is: ", stacks[i], " lengte is: ", len(stacks[i]))
+        text = stacks[i][len(stacks[i])-1]
+        print(" last digit is: ", text)
+        solution += text
+    return solution
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input"""
     data = parse(puzzle_input)
     solution1 = part1(data)
+    data = parse(puzzle_input)
     solution2 = part2(data)
 
     return solution1, solution2
@@ -68,6 +115,6 @@ def solve(puzzle_input):
 if __name__ == "__main__":
     for path in sys.argv[1:]:
         print(f"{path}:")
-        puzzle_input = pathlib.Path(path).read_text().strip()
+        puzzle_input = pathlib.Path(path).read_text()
         solutions = solve(puzzle_input)
         print("\n".join(str(solution) for solution in solutions))

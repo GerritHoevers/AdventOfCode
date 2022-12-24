2f49ca1f1955c591514433dcf25feddf39df3287
Gerrit Hoevers <gerrithoevers@ziggo.nl>
2022-12-24 17:50:47 +0100

    solved day 8 on day 24

--- 2022/08/code.py
+++ 2022/08/code.py
@@ -10,26 +10,97 @@ def parse(puzzle_input):
 
     lines = [text for text in puzzle_input.split('\n')]
     size = len(lines)
-    print('size = ', size)
-    grid = np.array
-        
-    for row in range(0, size):
-        for col in range(0, size):
-            tree_size = lines[row][col]
-            print('row, col: ', row, col, tree_size)
-            grid[row][col] = int(tree_size)
+    grid = np.zeros((size, size)) #initialise grid with zeros
     
-    print(grid)
+    #fill grid with data from input file
+    row = 0
+    for line in lines:
+        for col in range(0, len(line)):
+            num = int(line[col])
+            grid[row][col] = num   
+        row += 1
+
     return grid
-
-def part1(data):
+    
+def part1(grid):
     """Solve part 1"""
-    print("\n\nsolving part 1 ...")
+    print("\nsolving part 1 ...\n")
 
+    dim = len(grid[0])
+    visible_trees = 4*dim - 4
 
-def part2(data):
+    for row in range(1, dim-1):
+        for col in range(1, dim-1):
+            #check view from left
+            visible_from_left = True
+            for j in range(0,col):
+                if grid[row][j] >= grid[row][col]:
+                    visible_from_left = False 
+            #check view from top
+            visible_from_top = True
+            for i in range(0, row):
+                if grid[i][col] >= grid[row][col]:
+                    visible_from_top = False 
+            #check view from right
+            visible_from_right = True
+            for j in range(col+1, dim):
+                if grid[row][j] >= grid[row][col]:
+                    visible_from_right = False 
+            #check view from bottom
+            visible_from_bottom = True
+            for i in range(row+1, dim):
+                if grid[i][col] >= grid[row][col]:
+                    visible_from_bottom = False 
+
+            if visible_from_left or visible_from_top or visible_from_right or visible_from_bottom:
+                visible_trees += 1
+
+    return visible_trees
+
+def part2(grid):
     """Solve part 2"""
     print("\n\nsolving part 2 ...")
+
+    dim = len(grid[0])
+    
+    max_scenic_score = 0
+
+    for row in range(1, dim-1):
+        for col in range(1, dim-1):
+
+            #check view from left
+            visible_from_left = 0
+            for j in range(col-1, -1, -1):
+                visible_from_left += 1
+                if grid[row][j] >= grid[row][col]:
+                    break
+            
+            #check view from top
+            visible_from_top = 0
+            for i in range(row-1 ,-1 , -1):
+                visible_from_top += 1
+                if grid[i][col] >= grid[row][col]:
+                    break
+                 
+            #check view from right
+            visible_from_right = 0
+            for j in range(col+1, dim):
+                visible_from_right += 1
+                if grid[row][j] >= grid[row][col]:
+                    break
+
+            #check view from bottom
+            visible_from_bottom = 0
+            for i in range(row+1, dim):
+                visible_from_bottom += 1
+                if grid[i][col] >= grid[row][col]:
+                    break
+
+            scenic_score = visible_from_left * visible_from_top * visible_from_right * visible_from_bottom
+            if scenic_score > max_scenic_score:
+                max_scenic_score = scenic_score
+
+    return max_scenic_score
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input"""

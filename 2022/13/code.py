569316b22922debc9b9d58452f5bf2ea3f2f17f9
Gerrit Hoevers <gerrithoevers@ziggo.nl>
2023-01-08 21:26:08 +0100

    working ...

--- 2022/13/code.py
+++ 2022/13/code.py
@@ -5,36 +5,46 @@ import sys
 from datetime import datetime
 
 
-def convert(text):
-    # convert text to list of strings
+def get_elements(packet):
+    # get list of elements in packet
     elements = []
+    # skip leading '['
+    index_packet = 1
+    index = 0
+    char = packet[index_packet]
+
+
 
     return elements
 
 def parse(puzzle_input):
     """Parse input"""
 
-    data = []
+    data = [text for text in puzzle_input.split('\n')]
+    packets = []
     pair = 0
-    for text in puzzle_input.split('\n'):
-        if text != "":
-            data.append(text)
+    while (pair*3) < len(data):
+        packets.append(data[3*pair])
+        packets.append(data[3*pair+1])
+        pair += 1
 
-
-
-    return data
+    return packets
 
 def part1(data):
     """Solve part 1"""
     print("\nsolving part 1 ...")
 
-    #pairs = len(data)
-    pairs = 1
+    pairs = len(data) // 2
     for i in range(pairs):
-        left_packet = data[i][0]
-        right_packet = data[i][1]
+        left_packet = data[2*i]
+        right_packet = data[2*i+1]
         print("== Pair", i+1, "==")
-        print("- Compare ", left_packet, " vs ", right_packet)
+        print("- Compare", left_packet, " vs ", right_packet)
+        left_elements = get_elements(left_packet)
+        right_elements = get_elements(right_packet)
+        for j in range(len(left_elements)):
+            print("  - Compare", left_elements[j], "vs", right_elements[j])
+        print()
 
     return
 
